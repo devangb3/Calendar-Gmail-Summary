2fa74c9aa11508c68538c939c8f3b9b292d7368a
Devang Borkar <devangborkar3@gmail.com>
2025-04-20 06:37:52 -0700

    Add logo

--- frontend/src/components/SummaryPage.js
+++ frontend/src/components/SummaryPage.js
@@ -141,7 +141,7 @@ function SummaryPage() {
       <EnhancedHeader>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
           <Typography variant="h4" component="h1" sx={{ fontWeight: 700 }}>
-            Daily Summary
+            Daily Digest
           </Typography>
           <Box>
             <DatabaseStatus status={dbStatus} />
